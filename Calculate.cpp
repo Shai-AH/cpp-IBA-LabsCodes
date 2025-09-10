44e123f1548e6059879bb280971f51c63a8b66b9
Shai-AH <shahid.nazeer323@gmail.com>
2025-09-10 21:29:00 +0500

    Add Calculate.cpp
    Practice question used to Add and Multiply input Numbers...

---
+++ Calculate.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(){
+int a,b;
+cout << "Enter 2 Numbers";
+cin >> a >> b;
+int sum = a + b;
+int mult = a * b;
+cout << "After Addition= " << sum;
+cout << "After Multiplication= " << mult;
+}
